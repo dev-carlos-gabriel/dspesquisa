324e3c6a5bee2d675256d6d5e6fc0c4a719305b9
Carlos Gabriel <carlosgabriel632@gmail.com>
2020-09-14 22:28:03 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsupeior/pesquisa/PspesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.devsupeior.pesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsupeior/pesquisa/PspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsupeior.pesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
